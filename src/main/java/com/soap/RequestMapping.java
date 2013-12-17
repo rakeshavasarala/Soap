7b4b15fc6c958403d7bb563ad4220b634b0d7493
Rakesh Avasarala <Rakesh.Avasarala@aimia.com>
2013-12-17 16:40:03 +0000

    Amend gitignore

---
+++ src/main/java/com/soap/RequestMapping.java
@@ -0,0 +1,11 @@
+package com.soap;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Rakesh Avasarala
+ * Date: 12/17/13
+ * Time: 1:30 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public enum RequestMapping {
+}
